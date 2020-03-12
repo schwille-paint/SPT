a48bdbb8a824c8208bab36e7d4c4e5b48512d779
Florian Stehr <florian.stehr@gmail.com>
2020-03-12 11:51:59 +0100

    Multi level gaussian fitting in improps and express

--- spt/immobile_props.py
+++ spt/immobile_props.py
@@ -7,20 +7,25 @@ import dask.dataframe as dd
 import multiprocessing as mp
 import time
 import hdbscan
+from scipy.optimize import curve_fit
 import warnings
 warnings.filterwarnings("ignore")
 
 import picasso_addon.io as addon_io
+import spt.analytic_expressions as express
+
 
 #%%
 def get_trace(df,NoFrames):
     '''
     Get intensity vs time trace of length=NoFrames for group.
     '''
-    df['photons']=df['photons'].abs() # Get absolute values of photons
-    df_sum=df[['frame','photons']].groupby('frame').sum() # Sum multiple localizations in single frame
+    field='net_gradient'
+    
+    df[field]=df[field].abs() # Get absolute values of field
+    df_sum=df[['frame',field]].groupby('frame').sum() # Sum multiple localizations in single frame
     trace=np.zeros(NoFrames) # Define trace of length=NoFrames with zero entries
-    trace[df_sum.index.values]=df_sum['photons'].values # Add (summed) photons to trace for each frame
+    trace[df_sum.index.values]=df_sum[field].values # Add (summed) field entries to trace for each frame
     
     return trace
 
@@ -199,36 +204,148 @@ def get_var(df):
     return s_out
 
 #%%
-def center_normdistance(centers,k):
-    
-    d_norm=(centers[k+1:]-centers[k:-1])/centers[k]
-    
-    return d_norm
-#%%
-def get_levels(df):
+def cluster_levels(df):
     
     ### Prepare data
-    data=df.net_gradient.values.reshape(-1,1)
-    # data=data[np.random.randint(0,len(data),size=int(len(data)/2))]
+    data=df.net_gradient.values # Net gradient instead of photons yields levels with lower spread
+    
+    data=data[data<np.percentile(data,99)] # Cut off highest 0.5% values
+    data=data.reshape(-1,1)
+    
     ### We only define the minimum clustersize as input parameter to hdbscan
     min_cluster_size=int(len(data)*0.02)
     if min_cluster_size<50:
         min_cluster_size=50
+   
     ### Cluster
     clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size,
                                 allow_single_cluster=False,
                                 ).fit(data)
     
     cluster_id=clusterer.labels_ # Cluster ids
-    ### Get cluster centers, i.e. sorted medians (from lowest to highest ng value)
-    cluster_centers=[np.median(data[cluster_id==i]) for i in np.unique(cluster_id)[1:]]
-    cluster_centers.sort()
-    cluster_centers=np.asarray(cluster_centers)
-    ### Get cluster center distances normed to lowest cluster R0
-    dnorm0=center_normdistance(cluster_centers,0)
     
-    return data,cluster_centers,dnorm0
+    ### Get cluster centers and sizes, i.e. sorted after medians (from lowest to highest ng value)
+    cluster_centers=np.asarray([np.median(data[cluster_id==i]) for i in np.unique(cluster_id)[1:]]) # Medians
+    cluster_size=np.asarray([len(data[cluster_id==i]) for i in np.unique(cluster_id)[1:]]) # Sizes
+    clusters=np.vstack([cluster_centers,cluster_size]).T
     
+    clusters=clusters[np.argsort(clusters[:,0]),:] # Sort after medians
+
+    return data,clusters
+
+#%%
+def fit_Ncomb(x,y,centers_init,N):
+    
+    ### Prepare start parameters for fit
+    p0=[]
+    p0.extend([centers_init[0]])
+    p0.extend([p0[0]/4])
+    p0.extend([max(y) for i in range(N)])
+    
+    ### Define comb of N gaussians
+    def gauss_comb(x,*p): return express.gauss_Ncomb(x,p,N)
+    
+    def redchi(x,y,p):
+        yopt=gauss_comb(x,*p) # Get reduced chi
+        chi=np.divide((yopt-y)**2,y)
+        chi=chi[np.isfinite(chi)]
+        chi=np.sum(chi)/(len(chi)-len(p))
+        return chi
+    
+    ### Fit
+    levels=np.zeros(4+6)
+    try:
+        ### Try first cluster peak as start parameter
+        popt0,pcov=curve_fit(gauss_comb,x,y,p0=p0)
+        popt0=np.absolute(popt0)
+        
+        ### Try second cluster peak as start parameter
+        p0[0]=centers_init[1]
+        popt1,pcov=curve_fit(gauss_comb,x,y,p0=p0)
+        popt1=np.absolute(popt1)
+        
+        ### Select better chi fit
+        if redchi(x,y,popt0)<redchi(x,y,popt1): popt=popt0
+        else: popt=popt1
+        
+        ### Try half of first cluster peak
+        p0[0]=centers_init[0]/2
+        popt3,pcov=curve_fit(gauss_comb,x,y,p0=p0)
+        popt3=np.absolute(popt3)
+        
+        ### Select better chi fit
+        if redchi(x,y,popt3)<redchi(x,y,popt): popt=popt3
+        
+    except:
+        levels[0]=N
+        levels[1]=np.nan # set chi to nan
+        levels[2:2+len(p0)]=np.zeros(len(p0)) # set popt to zeros
+        return levels
+   
+    ### Remove peaks outside of data range
+    centers=np.array([(i+1)*popt[0] for i in range(N)]) # Location of ith peak
+    popt[2:][centers>=max(x)]=0 #Assign zeros to out of range
+    
+    ### Set peaks with an amplitude lower than 1% than the peak amplitude to zero
+    Acrit=0.01*max(popt[2:])
+    popt[2:][popt[2:]<Acrit]=0
+     
+    ### Now calculate fit reduced chi
+    chi=redchi(x,y,popt)
+    
+    ### Assign to end result
+    levels[0]=N
+    levels[1]=chi
+    levels[2:2+len(popt)]=popt
+    
+    return levels
+        
+#%%
+def fit_levels(data,centers_init):
+     
+    ### Prepare data for fit
+    y,x=np.histogram(data,
+                     bins='auto',
+                     )
+    x=x[:-1]+(x[1]-x[0])/2
+    
+    ### Fit comb of Gaussians for different number of levels
+    fits=np.zeros((10,6))
+    for N in range(1,7):
+        fits[:,N-1]=fit_Ncomb(x,y,centers_init,N) 
+    
+    ########## Decide on the appropriate number of levels based on following criteria
+    levels=fits.copy()
+    
+    ### 1. Set chi value of fits having two extrema within peak amplitudes to NaN
+    for i in range(np.shape(levels)[1]):
+        diff_sign=np.sign(np.diff(levels[4:,i]))
+        diff_sign=diff_sign[diff_sign!=0]
+        n_maxima=np.sum(np.absolute(np.diff(diff_sign))==2)
+        if n_maxima>2: levels[1,i]=np.nan
+    
+    ### 2. Remove NaNs in chi, corresponding to not succesful fits
+    levels=levels[:,np.isfinite(levels[1,:])]
+    
+    ### 3. Search for k with minimum reduced chisquare, go back in ks and return value where first jump bigger than 10% occurs
+    k=np.argmin(levels[1,:])
+    chis=levels[1,:]
+    if k>0:
+        for i in range(k,0,-1):
+            if np.absolute((chis[i]-chis[i-1])/chis[i-1])>0.1:
+                k=i
+                break
+    
+    ### After decision was made prepare return
+    N=int(levels[0,k])
+    
+    pN=levels[2:2+2+N,k]
+    yopt=express.gauss_Ncomb(x,pN,N)
+    
+    N=np.sum(levels[4:,k]>0)
+    
+    return N,x,y,yopt,fits
+
 #%%
 def get_props(df,ignore=1):
     """ 
