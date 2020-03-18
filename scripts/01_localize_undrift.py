4da9b4463187e0c1b4f4446ef0642d333d5728a2
Florian Stehr <florian.stehr@gmail.com>
2020-03-18 14:57:33 +0100

    Change in script 01 due to change in picasso_addon

--- scripts/01_localize_undrift.py
+++ scripts/01_localize_undrift.py
@@ -1,5 +1,6 @@
 #Script to call picasso_addon.localize.main()
 import os
+import traceback
 import importlib
 import matplotlib.pyplot as plt
 
@@ -10,16 +11,16 @@ import picasso_addon.localize as localize
 importlib.reload(localize)
 ############################################# Load raw data
 dir_names=[]
-dir_names.extend(['/fs/pool/pool-schwille-paint/Data/p06.SP-tracking/19-12-16_SLB_newstock_fix_vs_th/s2_LC+Str+40nM-R1S1-8_exp100_p114uW_1']*2)
+dir_names.extend([r'C:\Data\p06.SP-tracking\20-01-16_immob-th_pseries_livecell\id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1'])
 
 file_names=[]
-file_names.extend(['s2_LC+Str+40nM-R1S1-8_exp100_p114uW_1_MMStack_Pos1.ome.tif'])
-file_names.extend(['s2_LC+Str+40nM-R1S1-8_exp100_p114uW_1_MMStack_Pos2.ome.tif'])
+file_names.extend(['id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1_MMStack_Pos0.ome_locs.hdf5'])
 
 ############################################ Set non standard parameters 
 ### Valid for all evaluations
-params_all={'undrift':False,
-            'mng':1000}
+params_all={'localize':False,
+            }
+
 ### Exceptions
 params_special={}
 
@@ -33,11 +34,17 @@ for i in range(0,len(file_names)):
     params=params_all.copy()
     for key, value in params_special.items():
         params[key]=value[i]
+    
     ### Run main function
     try:
-        movie,info=io.load_movie(path)
-        out=localize.main(movie,info,**params)
-    except:
+        try:
+            file,info=io.load_movie(path) # Load movie
+        except:
+            file,info=io.load_locs(path) # Load _locs            
+        out=localize.main(file,info,path,**params)    
+    
+    except Exception:
+        traceback.print_exc()
         failed_path.extend([path])
 
 print()    
@@ -52,26 +59,26 @@ print('Failed attempts: %i'%(len(failed_path)))
 ############################################ Checkout single files
 
 ### Load movie
-i=0
-path=os.path.join(dir_names[i],file_names[i])
-movie,info=io.load_movie(path)
-#%%
-### Spot detection
-frame=10
-params={'mng':1000,
-        'box':5}
+# i=0
+# path=os.path.join(dir_names[i],file_names[i])
+# movie,info=io.load_movie(path)
+# #%%
+# ### Spot detection
+# frame=10
+# params={'mng':1000,
+#         'box':5}
 
-y,x,ng= picasso.localize.identify_in_frame(movie[frame],
-                                           params['mng'],
-                                           params['box'])
-### Preview
-f=plt.figure(num=1,figsize=[4,4])
-f.subplots_adjust(bottom=0.,top=1.,left=0.,right=1.)
-f.clear()
-ax=f.add_subplot(111)
-ax.imshow(movie[frame],cmap='gray',vmin=60,vmax=400,interpolation='nearest',origin='lower')
-ax.scatter(x,y,s=150,marker='o',color='None',edgecolor='r',lw=2)   
+# y,x,ng= picasso.localize.identify_in_frame(movie[frame],
+#                                            params['mng'],
+#                                            params['box'])
+# ### Preview
+# f=plt.figure(num=1,figsize=[4,4])
+# f.subplots_adjust(bottom=0.,top=1.,left=0.,right=1.)
+# f.clear()
+# ax=f.add_subplot(111)
+# ax.imshow(movie[frame],cmap='gray',vmin=60,vmax=400,interpolation='nearest',origin='lower')
+# ax.scatter(x,y,s=150,marker='o',color='None',edgecolor='r',lw=2)   
 
-ax.grid(False)
-ax.set_xlim(200,400)
-ax.set_ylim(200,400)
+# ax.grid(False)
+# ax.set_xlim(200,400)
+# ax.set_ylim(200,400)
--- scripts/02_autopick.py
+++ scripts/02_autopick.py
@@ -1,5 +1,6 @@
 #Script to call picass_addon.autopick main()
 import os
+import traceback
 import importlib
 import matplotlib.pyplot as plt
 
@@ -11,14 +12,14 @@ importlib.reload(autopick)
 
 ############################################# Load raw data
 dir_names=[]
-dir_names.extend(['directory to locs_render file'])
+dir_names.extend([r'C:\Data\p06.SP-tracking\20-01-16_immob-th_pseries_livecell\id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1'])
 
 file_names=[]
-file_names.extend(['file_name'])
+file_names.extend(['id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1_MMStack_Pos0.ome_locs_render.hdf5'])
 
 ############################################ Set non standard parameters 
 ### Valid for all evaluations
-params_all={}
+params_all={'lbfcs':True}
 ## Exceptions
 params_special={}
 
@@ -34,8 +35,9 @@ for i in range(0,len(file_names)):
     ### Run main function
     try:
         locs,info=io.load_locs(path)
-        out=autopick.main(locs,info,**params)
-    except:
+        out=autopick.main(locs,info,path,**params)
+    except Exception:
+        traceback.print_exc()
         failed_path.extend([path])
 
 print()    
@@ -47,42 +49,42 @@ print('Failed attempts: %i'%(len(failed_path)))
 
 #%%
 ############################################ Checkout single files
-i=0
+# i=0
 
-params={'oversampling':5,
-        'pick_box':11, # Usually 2*oversampling+1
-        'min_n_locs':100,
-        'fit_center':False,
-        'pick_diameter':2,
-        }
+# params={'oversampling':5,
+#         'pick_box':11, # Usually 2*oversampling+1
+#         'min_n_locs':100,
+#         'fit_center':False,
+#         'pick_diameter':2,
+#         }
 
-### Load file
-path=os.path.join(dir_names[i],file_names[i])
-locs,info=io.load_locs(path)
-### Render
-image=render.render(locs,
-                    info,
-                    params['oversampling'],
-                    )[1] 
-### Pick detection
-centers=autopick.spotcenters_in_image(image,
-                                      params['pick_box'],
-                                      params['min_n_locs'],
-                                      params['fit_center'])[0]
-#%%
-### Preview
-f=plt.figure(num=1,figsize=[4,4])
-f.subplots_adjust(bottom=0.,top=1.,left=0.,right=1.)
-f.clear()
-ax=f.add_subplot(111)
+# ### Load file
+# path=os.path.join(dir_names[i],file_names[i])
+# locs,info=io.load_locs(path)
+# ### Render
+# image=render.render(locs,
+#                     info,
+#                     params['oversampling'],
+#                     )[1] 
+# ### Pick detection
+# centers=autopick.spotcenters_in_image(image,
+#                                       params['pick_box'],
+#                                       params['min_n_locs'],
+#                                       params['fit_center'])[0]
+# #%%
+# ### Preview
+# f=plt.figure(num=1,figsize=[4,4])
+# f.subplots_adjust(bottom=0.,top=1.,left=0.,right=1.)
+# f.clear()
+# ax=f.add_subplot(111)
 
-ax.imshow(image,cmap='gray',vmin=0,vmax=100,interpolation='nearest',origin='lower')
-for i in range(0,len(centers)):
-    circle=plt.Circle((centers.loc[i,'x'],centers.loc[i,'y']),
-               params['pick_diameter']/2*params['oversampling'],
-               facecolor='None',
-               edgecolor='y',
-               lw=2)
-    ax.add_artist(circle)
+# ax.imshow(image,cmap='gray',vmin=0,vmax=100,interpolation='nearest',origin='lower')
+# for i in range(0,len(centers)):
+#     circle=plt.Circle((centers.loc[i,'x'],centers.loc[i,'y']),
+#                params['pick_diameter']/2*params['oversampling'],
+#                facecolor='None',
+#                edgecolor='y',
+#                lw=2)
+#     ax.add_artist(circle)
 
-ax.grid(False)
+# ax.grid(False)
