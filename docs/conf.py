b3fb4b71582183ae4b2555e2c940012ab53a3825
FlorianStehr <florian.stehr@gmail.com>
2020-06-02 15:30:03 +0200

    RTD: Build failed changed requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,13 @@ release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
