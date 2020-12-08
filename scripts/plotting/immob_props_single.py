e2afe378275c95db3d8e3ac104bc203d3bf7849d
Florian Stehr <florian.stehr@gmail.com>
2020-12-08 12:24:02 +0100

    Query for groups in immob_props_single

--- scripts/plotting/immob_props_single.py
+++ scripts/plotting/immob_props_single.py
@@ -13,26 +13,29 @@ importlib.reload(improps)
 plt.style.use('~/lbFCS/styles/paper.mplstyle')
 ############################################# Load raw data
 dir_names=[]
-dir_names.extend([r'C:\Data\p06.SP-tracking\20-01-17_th_slb_L_T21\slb_id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1']*2)
+dir_names.extend(['/fs/pool/pool-schwille-spt/Data/D042_TIRFM/20201125_GAB/Trackinghandle_ID183_a20_40nM_Pm2Cy3B_20uW_1']*2)
 
 file_names=[]
-file_names.extend(['slb_id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1_MMStack_Pos0.ome_locs_picked0503_tmobprops.hdf5'])
-file_names.extend(['slb_id169_R1-54#_R1s1-8_40nM_exp200_p114uW_T21_1_MMStack_Pos0.ome_locs_picked0503_split50_tmobprops.hdf5'])
+file_names.extend(['Trackinghandle_ID183_a20_40nM_Pm2Cy3B_20uW_1_MMStack_Pos0.ome_locs_render_picked.hdf5'])
+file_names.extend(['Trackinghandle_ID183_a20_40nM_Pm2Cy3B_20uW_1_MMStack_Pos0.ome_locs_render_picked_tprops.hdf5'])
 
-############################################ Set parameters for call to spt.immob_props.main() and dataset
-params={'filter':'fix'} # Parameters
-i=0 # Select dataset
+############################################ Set parameters
 CycleTime=0.2 #Aquisition cycle time [s]
 
 #%%
-############################################ Run spt.immob_props.main()               
-path=os.path.join(dir_names[i],file_names[i]) # Path
-locs,info=addon_io.load_locs(path) # Load data
-out=improps.main(locs,info,**params) # Run main
+############################################ Load _picked and _tprops            
+paths = [os.path.join(dir_names[i],file_name) for i, file_name in enumerate(file_names)]
+locs_init,info=addon_io.load_locs(paths[0]) # Load _picked
+props,info=addon_io.load_locs(paths[1]) # Load _tprops
+
+#%%
+############################################ Query _picked for groups in _tprops
+groups = props.group.values
+locs = locs_init.query('group in @groups')
 
 #%%
 ############################################ Select one group
-g=3
+g=102
 picked=locs[locs.group==g]
 
 ### Trace
@@ -44,7 +47,7 @@ idx=list(gT.index.values)
 T=np.unique([int(i[1:]) for i in idx]) # Index
 
 ### gT (ensemble)
-gT_mean,T=analyze.get_NgT(out[1])
+gT_mean,T=analyze.get_NgT(props)
 
 
 ############################################ Plotting
@@ -59,12 +62,7 @@ ax.plot(np.arange(0,len(trace),1)*CycleTime,
         c='b',
         lw=2)
 
-ax.set_xlim(0,120)
-ax.set_xticks([0,30,60,90,120])
 ax.set_xlabel('Time (s)')
-
-ax.set_ylim(-0.5,7)
-ax.set_yticks([0,3,6])
 ax.set_ylabel('#Photons x100')
 
 
@@ -89,20 +87,14 @@ ax.plot(T*CycleTime,
         lw=2,
         label='ensemble')
 
-# ax.plot(T*CycleTime,
-#         gT_mean.loc[idx,'50%'],
-#         '--',
-#         c='r',
-#         lw=2,
-#         label='ensemble')
 
 ax.set_xscale('log')
-ax.set_xlim(0.2,120)
+ax.set_xlim(0.2,1e3)
 ax.set_xticks([1,10,100])
 ax.set_xlabel('Time (s)')
 
-# ax.set_yscale('log')
-ax.set_ylim(0.1,10)
-ax.set_ylabel(r'N($\tau\geq T$)')
+ax.set_yscale('log')
+ax.set_ylim(0.1,1e3)
+ax.set_ylabel(r'TPP($\tau\geq T$)')
 
 ax.legend()
