e48c10402ae65b284acd9d049472a278462fe4cd
FlorianStehr <florian.stehr@gmail.com>
2021-08-08 18:30:47 +0200

    BUGFIX in requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Florian Stehr'
 author = 'Florian Stehr'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
